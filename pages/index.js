45f45d69c290677da906d2f2380dcccc49459a07
Prasanjit-4 <Prasanjit-4@users.noreply.github.com>
2023-11-04 17:42:02 +0530

    simple portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,57 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
+      <section className={utilStyles.headingMd}>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1>About Me</h1>
+        <p>I'm a second year student at SRM Institute of Science and Technology pursuing BTech. CSE with specialization in cloud computing.</p>
+        <p>Currently I am learning about blockchain infrastructure along with the cryptographic principles that supports it all.</p>
+        <p>My interests include playing football, reading books and learning mathematics</p>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+        <h1>My Socials</h1>
+        <p>
+          <a href="https://github.com/Prasanjit-4" target='_blank'>Github Profile</a>
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/prasanjit-panda-56152a250/" target="_blank">Linkedin Profile</a>
+        </p>
+        <p>
+          <a href="https://devfolio.co/@peajay" target="_blank">Devfolio Profile</a>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <h1>Projects</h1>
+        <p><a href='https://github.com/stuff-makers/Polinyx' target="_blank">Polinyx</a></p>
+        <p>A video chatting application with sign language interpretation and Speech-to-text features</p>
+        <p>
+          <a href="https://github.com/vinay-04/unfold-precompile-evm" target="_blank">ZkSnark Precompile</a>
+        </p>
+        <p>A ZkSnark based precompile for the Avalanche network</p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <h1>Hackathons</h1>
+        <p>I love participating in hackathons. They are a great platform to learn about new technologies in a short period of time and ofcourse get in contact with other like-minded individuals</p>
+        <p>Throughout my 3rd semester I participated in various hackathons and advanced to their later stages unfortunately didn't win a lot.</p>
+        <p>Recently I participated in <a href='https://unfoldweb3.com/#home'>Unfold 2023</a> and was able to win <a href="https://x.com/Avalanche_In/status/1717543567571464642?s=20">3rd Position</a> in the Avalanche Track along with a prize amount of $1000</p>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        <h1>Academics</h1>
+        <p>Semester wise GPA</p>
+        <p>Semester 1: 9.86</p>
+        <p>Semester 2: 9.86</p>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+        <h1>Research Based Projects</h1>
+        <p>I created a small audio device using arduino that takes in audio input and analyzes it using Discrete Fourier Transform implemented using MATLAB.
+          This method of audio analysis has various applications such as detection of Parkinson's disease along with voice-based authentication.
+          This project won the TechKnow physics Exhibition organized by my college in my 2nd semester.
+        </p>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      </section>
+    </Layout>
   );
 }
